89630b8d73d4eb5a079dce2ea0ffe8ea447fe3b2
Fenosoa SOLOFO <solofoniainafenosoa020@gmail.com>
2025-02-09 19:44:10 +0300

    remove : all not used file removes

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Fugiat eum itaque eius nemo doloremque perferendis dicta eaque qui voluptatem quo,
+        consequatur placeat dolor cupiditate quis, sint enim porro et necessitatibus?
+      </p>
+    </>
+  );
+}
+
+export default App;
